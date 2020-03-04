851ff3bbdc70cfc62c763b8a49dde816dcf0ac84
Divaon <56230474+Divaon@users.noreply.github.com>
2020-03-04 21:48:17 +0300

    Add files via upload

---
+++ ISPLABS_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class human
+{
+
+    public string fio;
+    public int year;
+}
+
+class student
+{
+
+    public int curse;
+    public string tipe_educ;
+    public human ff=new human();
+
+}
+
+
+class stud_spesiation
+{
+
+    public string spec;
+    public student st= new student();
+}
+
+
+namespace ISPLAB3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ISP_LAB");
+            stud_spesiation std = new stud_spesiation();
+
+            std.spec = "KSIS";
+
+            std.st.tipe_educ = "free";
+            std.st.ff.fio = "Andrey Viktor Agibailov";
+            std.st.ff.year = 1975;
+            std.st.curse = 4;
+            Console.WriteLine(std.spec);
+            Console.WriteLine(std.st.curse);
+            Console.WriteLine(std.st.tipe_educ);
+            Console.WriteLine(std.st.ff.fio);
+            Console.WriteLine(std.st.ff.year);
+
+        }
+    }
+}
